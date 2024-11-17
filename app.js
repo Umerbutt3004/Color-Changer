5c14630865da1442678493afca2659bdc21e6a9b
Umerbutt3004 <161926082+Umerbutt3004@users.noreply.github.com>
2024-11-17 03:37:04 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+ 
+function darkMode(){
+    document.querySelector("body").style.background="black";
+    document.body.style.color="white";
+};
+
+function whiteMode() {
+    document.body.style.background='white';
+    document.body.style.color='blue';
+};
